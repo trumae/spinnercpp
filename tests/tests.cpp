7bb38035b22dbcefc0c102033cfed06985f84f87
Vinicius Valente Maciel <vvmaciel@outlook.com>
2019-10-04 14:26:29 -0300

    README.org

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -5,23 +5,6 @@
 
 using namespace std;
 
-TEST_CASE("simple working") {
-  int a = 5;
-  int b = 5;
-  //
-  //CHECK(throw_if(true, std::runtime_error("whops!")) == 42);
-  //
-  //	CHECK_FALSE(!(a == b));
-  //
-  REQUIRE(a == b);
-  
-  CHECK_EQ(a, b);
-  
-  //	CHECK(doctest::Approx(0.1000001) == 0.1000002);
-  //	CHECK(doctest::Approx(0.502) == 0.501);
-}
-
-
 TEST_CASE("spinnercpp") {
   {
     auto spin = std::make_unique<spinnercpp::spinner>(100ms, 10, "final msg", "> ", " |");
