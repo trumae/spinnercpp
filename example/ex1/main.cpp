8296f17697708bfdd373a68543061f9e8e03db31
Ivo Strejc <strejivo@fit.cvut.cz>
2019-10-05 19:58:49 +0200

    Reformat, changed int to size_t, updated "sub-CMakeLists" to include spinnercpp header, changed chars to reference

--- example/ex1/main.cpp
+++ example/ex1/main.cpp
@@ -1,9 +1,11 @@
 #include "../../spinnercpp.h"
 
+using std::chrono_literals::operator ""ms;
+
 int main() {
-  auto spin = std::make_unique<spinnercpp::spinner>();
-  spin->start();
-  std::this_thread::sleep_for(std::chrono::milliseconds(3000)); 
-  
-  return 0;
+	auto spin = std::make_unique<spinnercpp::spinner>(500ms);
+	spin->start();
+	std::this_thread::sleep_for(std::chrono::milliseconds(8000));
+
+	return 0;
 }
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,21 +1,22 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
 #include "doctest.h"
 
 #include "../spinnercpp.h"
 
 using namespace std;
 
-TEST_CASE("spinnercpp") {
-  {
-    auto spin = std::make_unique<spinnercpp::spinner>(100ms, 10, "final msg", "> ", " |");
-    spin->start();
-    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-  }
-  
-  for(int i = 0 ; i < spinnercpp::CharSets.size(); i++){
-    cout << endl;
-    auto spin = std::make_unique<spinnercpp::spinner>(100ms, i);
-    spin->start();
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-  }
+TEST_CASE ("spinnercpp") {
+	{
+		auto spin = std::make_unique<spinnercpp::spinner>(100ms, 10, "final msg", "> ", " |");
+		spin->start();
+		std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+	}
+
+	for (int i = 0; i < spinnercpp::CharSets.size(); i++) {
+		cout << endl;
+		auto spin = std::make_unique<spinnercpp::spinner>(100ms, i);
+		spin->start();
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+	}
 }
